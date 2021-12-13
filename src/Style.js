f83ef086332965db8bdc53b9ec10837d090aa8fa
biscrab <biscrab@naver.com>
2021-12-13 18:47:58 +0900

    완성

---
+++ src/Style.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+    width: 100%;
+    background-color: #98d977;
+    padding: 15px 20px;
+    color: white;
+    font-weight: bold;
+    font-size: 25px;
+`
+
+export const Body = styled.div`
+    width: 1200px;
+    position: relative;
+    display: flex;
+    //align-items: center;
+    flex-direction: column;
+    line-height: 30px;
+    left: 50%;
+    transform: translateX(-50%);
+    padding-bottom: 100px;
+
+    @media screen and (max-width: 1200px){
+        width: 100%;
+        padding: 20px;
+        left: 0;
+        transform: translateX(0%);
+    }
+
+    div{
+        display: flex;
+    }
+`
+
+export const HL = styled.b`
+    padding: 0px 5px;
+    margin-left: 10px;
+    :before{
+        content: "-> "
+    }
+`
